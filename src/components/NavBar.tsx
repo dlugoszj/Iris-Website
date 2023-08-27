2a8a89bff9a9909bf1e473642ec536f211f1ee0e
dlugoszj <dlugoszj@umich.edu>
2023-08-27 19:15:15 -0400

    NavBar styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,26 @@
-import { HStack, Text } from "@chakra-ui/react";
+import { HStack, Text, Box, Center } from "@chakra-ui/react";
 import React from "react";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent={"space-between"} padding="10px">
-      <Text>Yup</Text>
-    </HStack>
+    <Center>
+      <Box
+        background="#fff0f5"
+        w="90%"
+        p={4}
+        color="black"
+        rounded="xl"
+        marginTop="35px"
+      >
+        <HStack justifyContent={"space-between"} padding="10px">
+          <Text>Home</Text>
+          <Text>Effect Library</Text>
+          <Text>Logo</Text>
+          <Text>Effect Builder</Text>
+          <Text>Account</Text>
+        </HStack>
+      </Box>
+    </Center>
   );
 };
 
