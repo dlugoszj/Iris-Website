822ae5e9e70d5dbf2d699647b30bbfb0a783a676
dlugoszj <dlugoszj@umich.edu>
2023-09-02 15:51:24 -0400

    Nav Bar work

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,31 +1,35 @@
-import { HStack, Box, Center } from "@chakra-ui/react";
+import { HStack, Flex, Button, IconButton } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import React from "react";
 import { Link } from "react-router-dom";
+import "./components.css";
 
 const NavBar = () => {
   return (
     <>
-      <Center>
-        <Box
-          background="#fff0f5"
-          w="90%"
-          p={4}
-          color="black"
-          rounded="xl"
-          marginTop="35px"
-        >
-          <HStack justifyContent={"space-between"} padding="10px">
-            <Link to="/"> Home </Link>
-            <Link to="/Marketplace"> Effect Library </Link>
-            <Link to="/">
-              {" "}
-              <img src="/src/assets/Iris-Logo.png" />
-            </Link>
-            <Link to="/"> Effect Builder </Link>
-            <Link to="/"> Account </Link>
-          </HStack>
-        </Box>
-      </Center>
+      <Flex bg="#fff0f5" w="100%" px="6" align="center" justify="space-between">
+        <Link to="/">
+          {" "}
+          <img width="75px" height="75px" src="/src/assets/Iris-Logo.png" />
+        </Link>
+
+        <HStack hideBelow={"lg"} spacing="150px">
+          <Link to="/"> Home </Link>
+          <Link to="/Marketplace"> Effect Library </Link>
+          <Link to="/"> Effect Builder </Link>
+          <Link to="/"> About </Link>
+        </HStack>
+
+        <HStack>
+          <IconButton
+            aria-label={"Open Menu"}
+            size="md"
+            icon={<HamburgerIcon />}
+            hideFrom={"lg"}
+          />
+          <Button>Sign In</Button>
+        </HStack>
+      </Flex>
     </>
   );
 };
