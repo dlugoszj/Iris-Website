28945c498f053fbae3e4c482772f5f0bc221c650
dlugoszj <dlugoszj@umich.edu>
2023-09-14 19:13:27 -0400

    Added font and some styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,17 +2,20 @@ import { HStack, Flex, Button } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 import HamburgerMenu from "./HamburgerMenu";
+import "/src/assets/css/fonts.css";
 
 const NavBar = () => {
   return (
     <>
       <Flex
-        bg="#1b1b1b"
+        bg="black"
         w="100%"
         px="6"
         align="center"
         justify="space-between"
         position="fixed"
+        borderBottom="1px"
+        borderBottomColor="#282828"
       >
         <Link to="/">
           {" "}
@@ -20,15 +23,27 @@ const NavBar = () => {
         </Link>
 
         <HStack hideBelow={"lg"} spacing="150px" color="white">
-          <Link to="/"> Home </Link>
-          <Link to="/Marketplace"> Effect Library </Link>
-          <Link to="/"> Effect Builder </Link>
-          <Link to="/"> About </Link>
+          <Link to="/" className="font">
+            {" "}
+            Home{" "}
+          </Link>
+          <Link to="/Marketplace" className="font">
+            {" "}
+            Effect Library{" "}
+          </Link>
+          <Link to="/" className="font">
+            {" "}
+            Effect Builder{" "}
+          </Link>
+          <Link to="/" className="font">
+            {" "}
+            About{" "}
+          </Link>
         </HStack>
 
         <HStack>
           <HamburgerMenu />
-          <Button>Sign In</Button>
+          <Button className="font">Sign In</Button>
         </HStack>
       </Flex>
     </>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "../../components/NavBar";
 import { Grid, GridItem, Text } from "@chakra-ui/react";
+import "/src/assets/css/fonts.css";
 
 const Home = () => {
   return (
@@ -8,31 +9,53 @@ const Home = () => {
       <GridItem>
         <NavBar />
       </GridItem>
-      <GridItem w="100vw" h="auto" bg="#1b1b1b" marginTop={75}>
+      <GridItem w="100vw" h="auto" bg="black" marginTop={75}>
         <Text
           align="center"
-          fontSize={{ base: "5xl", md: "8xl", lg: "9xl" }}
+          fontSize={{ base: "4xl", md: "7xl", lg: "8xl" }}
           backgroundClip="text"
           color="transparent"
           bgGradient="linear-gradient(to right, #FF1CBF, #00FFE0)"
           m="auto"
           w="75vw"
+          className="font"
+          paddingTop="100px"
         >
           Illuminate your life
         </Text>
-        <Text align="center" fontSize={{ base: "xl", md: "5xl" }} color="white">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <Text
+          align="center"
+          fontSize={{ base: "xl", md: "5xl" }}
+          color="white"
+          className="font"
+        >
           Iris empowers you to
         </Text>
-        <Text align="center" fontSize={{ base: "xl", md: "5xl" }} color="white">
+        <Text
+          align="center"
+          fontSize={{ base: "xl", md: "5xl" }}
+          color="white"
+          className="font"
+        >
           {" "}
           take control and design
         </Text>
-        <Text align="center" fontSize={{ base: "xl", md: "5xl" }} color="white">
+        <Text
+          align="center"
+          fontSize={{ base: "xl", md: "5xl" }}
+          color="white"
+          className="font"
+        >
           {" "}
           LEDs the way you want.
         </Text>
       </GridItem>
-      <GridItem h="500" bg="#1b1b1b">
+      <GridItem h="500" bg="black">
         <Text>Hi Part 2</Text>
       </GridItem>
     </Grid>
