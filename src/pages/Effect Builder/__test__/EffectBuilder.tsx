67e80f7157f459f80292e2ed239c6718fe3afa69
dlugoszj <dlugoszj@umich.edu>
2023-09-28 20:11:59 -0400

    Effect Builder

--- src/pages/Effect Builder/__test__/EffectBuilder.tsx
+++ src/pages/Effect Builder/__test__/EffectBuilder.tsx
@@ -1,7 +1,26 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 import React from "react";
+import NavBar from "../../../components/NavBar";
 
 const EffectBuilder = () => {
-  return <div>EffectBuilder</div>;
+  return (
+    <Grid
+      templateAreas={`"navbar navbar"
+    "room editor"
+    `}
+      templateColumns="repeat(2, 1fr)"
+    >
+      <GridItem area={"navbar"}>
+        <NavBar />
+      </GridItem>
+      <GridItem pl="2" bg="pink.300" area={"room"} marginTop={75}>
+        Room
+      </GridItem>
+      <GridItem pl="2" bg="orange.300" area={"editor"} marginTop={75}>
+        Editor
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default EffectBuilder;
