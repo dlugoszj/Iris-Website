219401e078ed2e4995eeffbf61b398ed9727f685
dlugoszj <dlugoszj@umich.edu>
2023-10-15 20:34:17 -0400

    Add Layer

--- src/pages/Effect Builder/ActionManager/ActionAddLayer.tsx
+++ src/pages/Effect Builder/ActionManager/ActionAddLayer.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ActionOverviewTileContainer from "./ActionOverviewTileContainer";
-import "./ActionOverviewContainer.css"
+import "./ActionOverviewContainer.css";
 
 const ActionAddLayer = () => {
-
   React.useEffect(() => {
-
     window.addEventListener("resize", () => {
-      
       // Run this when the component mounts. It will allow us to dynamically set the width of the triangle
       const triangle = document.querySelector(".triangle") as HTMLElement;
       // set the width equal to the height
@@ -16,16 +13,16 @@ const ActionAddLayer = () => {
 
     return () => {
       // Code to run when component is unmounted. It will remove the event listener
-      window.removeEventListener("resize", () => {
-      });
+      window.removeEventListener("resize", () => {});
     };
   }, []);
 
   return (
     <ActionOverviewTileContainer>
-        <div className="triangle">
-            Add Layer
-        </div>
+      <div className="triangle">
+        <div className="add-layer">Add Layer</div>
+        <button className="plus">+</button>
+      </div>
     </ActionOverviewTileContainer>
   );
 };
